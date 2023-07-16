3add6d5ad69e894a21bcade42236008252ae266e
Sam Ess <samess.flowers@gmail.com>
2023-07-15 23:17:25 -0400

    Add dis/advantage and dropping lowest to roller.py

--- roller.py
+++ roller.py
@@ -34,8 +34,22 @@ def print_results(total:int, results:list) -> None:
 def main() -> None:
     '''Parse args, roll di(c)e'''
     parser = argparse.ArgumentParser()
-    parser.add_argument("dice")
+    parser.add_argument("dice", help="Something like 1d6", default="1d6", type=str)
+    options = parser.add_mutually_exclusive_group()
+    options.add_argument("-a", "--advantage", help="advantage", action="store_true")
+    options.add_argument("-d", "--disadvantage", help="disadvantage", action="store_true")
+    options.add_argument("-l", "--drop", "--lowest", "--drop-lowest", help="drop lowest", action="store_true")
     args = parser.parse_args()
+
+    if args.advantage:
+        result = max(roll(2,20)[1])
+        print("Rolling advantage: {result}")
+        sys.exit(0)
+    elif args.disadvantage:
+        result = min(roll(2,20)[1])
+        print("Rolling disadvantage: {result}")
+        sys.exit(0)
+
     try:
         dice = args.dice
         if len(dice) < 3 or 'd' not in dice:
@@ -44,18 +58,31 @@ def main() -> None:
         count, die = dice.split('d')
         count = int(count)
         die = int(die)
+
         if not isinstance(count,int) or not isinstance(die,int):
             print("type",type(count),type(die))
             raise DumbassError
-        print(f"Rolling {count} d{die}")
-        total, result = roll(count,die)
-        print_results(total, result)
+
+        if count < 2 and args.drop:
+            print("must drop two or more dice")
+
     except DumbassError:
         print(
-            "ERROR: Incorrect input, expected 1d4 or something like that",
+            "ERROR: Incorrect input, expected 1d6 or something like that",
             file=sys.stderr
             )
         sys.exit(10)
+    if args.drop:
+        print("Rolling {count} d{die}, dropping lowest")
+        total, result = roll(count,die)
+        result = sorted(result, reverse=True)
+        dropped = result[0:(len(result)-1)]
+        total = sum(dropped)
+        print_results(total,result)
+        sys.exit(0)
+    print(f"Rolling {count} d{die}")
+    total, result = roll(count,die)
+    print_results(total, result)
 
 if __name__ == "__main__":
     main()
